21eb04a9ddf2467bd4a32618e285b29c1ff2cb91
Rishant <rishantsingh0707@gmail.com>
2026-05-30 15:52:13 +0530

    test: auto-deployment workflow

--- backend/index.js
+++ backend/index.js
@@ -24,7 +24,7 @@ app.get('/api/data', (req, res) => {
   res.json({ message: 'This is data from the backend!' });
 });
 
-
+// Added for checking automation
 
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server is running on port ${PORT}`);
