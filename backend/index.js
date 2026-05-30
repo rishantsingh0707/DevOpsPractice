70158973d8b47a36dcd57136ab399308f6809a6e
Rishant <rishantsingh0707@gmail.com>
2026-05-30 13:24:41 +0530

    test auto-deploy

--- backend/index.js
+++ backend/index.js
@@ -24,6 +24,8 @@ app.get('/api/data', (req, res) => {
   res.json({ message: 'This is data from the backend!' });
 });
 
+
+
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server is running on port ${PORT}`);
 });
